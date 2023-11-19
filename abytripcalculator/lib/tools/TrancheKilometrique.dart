e122f93360b0a5d3761e589fced534cd947e70ae
othmane moujahid <othmane.mjhd@gmail.com>
2023-11-19 22:29:14 +0100

    Updates : clean of TrancheKilometrique

--- abytripcalculator/lib/tools/TrancheKilometrique.dart
+++ abytripcalculator/lib/tools/TrancheKilometrique.dart
@@ -8,17 +8,4 @@ class TrancheKilometrique {
 
 }
 
-/*void main() {
-  // Exemple d'utilisation de la classe TrancheKilometrique
-  TrancheKilometrique tranche1 = TrancheKilometrique(0, 100, 0.5);
-  TrancheKilometrique tranche2 = TrancheKilometrique(101, 200, 0.4);
-  TrancheKilometrique tranche3 = TrancheKilometrique(201, 300, 0.3);
 
-  // Vous pouvez maintenant stocker ces objets TrancheKilometrique dans une liste ou une autre structure selon vos besoins.
-  List<TrancheKilometrique> TrancheKilometriques = [tranche1, tranche2, tranche3];
-
-  // Exemple : Afficher les tranches et les frais de remboursement
-  for (TrancheKilometrique tranche in TrancheKilometriques) {
-    print('De ${tranche.debutTranche} à ${tranche.finTranche} km : ${tranche.fraisRemboursement} EUR/km');
-  }
-}*/
