d3473d337e224335a76a1a1c3f3e898b1a399c94
othmane moujahid <othmane.mjhd@gmail.com>
2023-11-23 13:09:07 +0100

    Update main to add the new page

--- abytripcalculator/lib/main.dart
+++ abytripcalculator/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:abytripcalculator/pages/calculateFundPage.dart';
 import 'package:abytripcalculator/pages/home.dart';
 import 'package:abytripcalculator/pages/loading.dart';
 import 'package:abytripcalculator/pages/login.dart';
+import 'package:abytripcalculator/pages/showDecomposition.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       '/home' : (context) => const HomePage(),
         '/login' : (context) => const LoginPage(),
         '/count' : (context) =>const CalculateFundPage(),
+        '/display' : (context) => const ShowDecomposition(),
     },
 
     );
