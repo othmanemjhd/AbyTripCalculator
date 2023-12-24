427acebd1f1ac0b4ec28c1ef4bd0eeb75506a351
othmane moujahid <othmane.mjhd@gmail.com>
2023-12-24 19:38:17 +0100

    Change login information

--- abytripcalculator/lib/services/SingleAuthService.dart
+++ abytripcalculator/lib/services/SingleAuthService.dart
@@ -4,8 +4,8 @@ class SingleAuthService {
 
   Future<bool> authenticate(String enteredUsername, String enteredPassword) async {
     // Replace 'your_username' and 'your_password' with your desired username and password
-    String validUsername = 'test';
-    String validPassword = 'test';
+    String validUsername = 'abylsen';
+    String validPassword = 'abylsen';
 
     if (enteredUsername == validUsername && enteredPassword == validPassword) {
       // Authentication successful
