891d1d44178cb550006001a1710d422c24bc0b4d
othmane moujahid <othmane.mjhd@gmail.com>
2023-12-27 02:30:53 +0100

    Add Gesture Detector

--- abytripcalculator/lib/pages/calculateFundPage.dart
+++ abytripcalculator/lib/pages/calculateFundPage.dart
@@ -24,6 +24,7 @@ class _CalculateFundPageState extends State<CalculateFundPage> {
 
   ];
   List<Map<String,dynamic>> map = [];
+  FocusNode _focusNode = FocusNode();
   void calculateFundsAndSetResult() {
     // Appel de la fonction avec la valeur actuelle de inputValue
     setState(() {
@@ -34,7 +35,12 @@ class _CalculateFundPageState extends State<CalculateFundPage> {
       map = instance.generateResultMap();
     });
   }
-
+  @override
+  void dispose() {
+    // Dispose of the FocusNode when the widget is disposed
+    _focusNode.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,179 +63,187 @@ class _CalculateFundPageState extends State<CalculateFundPage> {
 
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(30.0),
-              child: Center(
-                child: Text('BIENVENUE',style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                  fontFamily: 'Montserrat'
-                ),),
+        child: GestureDetector(
+          onTap: () {
+            // Call the unfocus method to hide the keyboard
+            _focusNode.unfocus();
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              
+              const Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Center(
+                  child: Text('BIENVENUE',style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    fontFamily: 'Montserrat'
+                  ),),
+                ),
               ),
-            ),
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 200.0,
-                enlargeCenterPage: true,
-                autoPlay: true,
-                aspectRatio: 16 / 9,
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 200.0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 16 / 9,
+                ),
+                items: _imageUrls.map((url) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Image.asset(
+                          url,
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
               ),
-              items: _imageUrls.map((url) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                      child: Image.asset(
-                        url,
-                        fit: BoxFit.cover,
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 20.0), // Ajustez cet espacement selon vos besoins
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: _formKey,
-                child: TextFormField(
-                  controller: _selectedNumberController,
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value?.isEmpty == true ) {
-                      return 'Ce champ est obligatoire';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    setState(() {
-                      _selectedNumber = int.tryParse(value) ?? 0;
-                    });
-                  },
-                  style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width * 0.05,
-                  ),
-                  decoration: const InputDecoration(
-                    labelText: 'Saisir le nombre de kilomètre global',
-                    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                    border: OutlineInputBorder(),
-
+              const SizedBox(height: 20.0), // Ajustez cet espacement selon vos besoins
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: TextFormField(
+                    focusNode: _focusNode,
+                    controller: _selectedNumberController,
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value?.isEmpty == true ) {
+                        return 'Ce champ est obligatoire';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedNumber = int.tryParse(value) ?? 0;
+                      });
+                    },
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width * 0.05,
+                    ),
+                    decoration: const InputDecoration(
+                      labelText: 'Saisir le nombre de kilomètre global',
+                      contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                      border: OutlineInputBorder(),
+          
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 10.0), // Ajustez cet espacement selon vos besoins
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0,horizontal: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Ajoutez ici la logique pour le bouton "Calculer"
-                        calculateFundsAndSetResult();
-                      }
-                    },
-                    style : ElevatedButton.styleFrom(
-                        padding: EdgeInsets.fromLTRB(
-                          MediaQuery.of(context).size.width * 0.1,
-                          MediaQuery.of(context).size.height * 0.015,
-                          MediaQuery.of(context).size.width * 0.1,
-                          MediaQuery.of(context).size.height * 0.015,
-                        ),
-                        backgroundColor: Colors.green[900],
-                        shape : RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0)
-                        )
-                    ),
-                    child: const Text('Calculer',style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat',
-                      color: Colors.white,
-                    ),),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      // Ajoutez ici la logique pour le bouton "Annuler"
-                      setState(() {
-                        _priceToRefund = 0;
-                        resultDecomposition = "";
-                        _selectedNumberController.clear();
-                        map.clear();
-                      });
-                    },
-                    style : ElevatedButton.styleFrom(
-                        backgroundColor: Colors.orange[200],
-                        padding: EdgeInsets.fromLTRB(
-                          MediaQuery.of(context).size.width * 0.1,
-                          MediaQuery.of(context).size.height * 0.015,
-                          MediaQuery.of(context).size.width * 0.1,
-                          MediaQuery.of(context).size.height * 0.015,
-                        ),
-                        shape : RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0)
-                        )
-                    ),
-                    child: const Text('Annuler',style : TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat',
-                      color: Colors.white,
-                    )),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 30.0),
+              const SizedBox(height: 10.0), // Ajustez cet espacement selon vos besoins
               Padding(
-                padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                padding: const EdgeInsets.symmetric(vertical: 0,horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text('Le montant à rembourser est : ${_priceToRefund.toStringAsFixed(2)} €',
-                            style: const TextStyle(
-                            fontFamily: 'montserrat',
-                            fontWeight: FontWeight.bold,
-                          ),),
-
-                   /* Text("Décomposition du calcul : $resultDecomposition",
-                      style: const TextStyle(
-                        fontFamily: 'montserrat',
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // Ajoutez ici la logique pour le bouton "Calculer"
+                          calculateFundsAndSetResult();
+                        }
+                      },
+                      style : ElevatedButton.styleFrom(
+                          padding: EdgeInsets.fromLTRB(
+                            MediaQuery.of(context).size.width * 0.1,
+                            MediaQuery.of(context).size.height * 0.015,
+                            MediaQuery.of(context).size.width * 0.1,
+                            MediaQuery.of(context).size.height * 0.015,
+                          ),
+                          backgroundColor: Colors.green[900],
+                          shape : RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)
+                          )
+                      ),
+                      child: const Text('Calculer',style: TextStyle(
                         fontWeight: FontWeight.bold,
-                      ),)*/
+                        fontFamily: 'Montserrat',
+                        color: Colors.white,
+                      ),),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Ajoutez ici la logique pour le bouton "Annuler"
+                        setState(() {
+                          _priceToRefund = 0;
+                          resultDecomposition = "";
+                          _selectedNumberController.clear();
+                          map.clear();
+                        });
+                      },
+                      style : ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orange[200],
+                          padding: EdgeInsets.fromLTRB(
+                            MediaQuery.of(context).size.width * 0.1,
+                            MediaQuery.of(context).size.height * 0.015,
+                            MediaQuery.of(context).size.width * 0.1,
+                            MediaQuery.of(context).size.height * 0.015,
+                          ),
+                          shape : RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)
+                          )
+                      ),
+                      child: const Text('Annuler',style : TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        color: Colors.white,
+                      )),
+                    ),
                   ],
                 ),
               ),
-            const SizedBox(height: 80.0),
-         Padding(
-           //padding: const EdgeInsets.all(8.0),
-           padding: EdgeInsets.fromLTRB(
-             MediaQuery.of(context).size.width * 0.25,
-             MediaQuery.of(context).size.height * 0.015,
-             MediaQuery.of(context).size.width * 0.25,
-             MediaQuery.of(context).size.height * 0.015,
+              const SizedBox(height: 30.0),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Le montant à rembourser est : ${_priceToRefund.toStringAsFixed(2)} €',
+                              style: const TextStyle(
+                              fontFamily: 'montserrat',
+                              fontWeight: FontWeight.bold,
+                            ),),
+          
+                     /* Text("Décomposition du calcul : $resultDecomposition",
+                        style: const TextStyle(
+                          fontFamily: 'montserrat',
+                          fontWeight: FontWeight.bold,
+                        ),)*/
+                    ],
+                  ),
+                ),
+              const SizedBox(height: 80.0),
+           Padding(
+             //padding: const EdgeInsets.all(8.0),
+             padding: EdgeInsets.fromLTRB(
+               MediaQuery.of(context).size.width * 0.25,
+               MediaQuery.of(context).size.height * 0.015,
+               MediaQuery.of(context).size.width * 0.25,
+               MediaQuery.of(context).size.height * 0.015,
+             ),
+             child: ElevatedButton(
+                 onPressed: () {
+                   Navigator.pushNamed(context, '/display', arguments: map);
+                   /*showDialog(
+                     context: context,
+                     builder: (BuildContext context) {
+                       return  MyDialog( map: map);
+                     },
+                   );*/
+                 },
+                 child: const Text("Décomposition",
+                     style: TextStyle(
+                         fontFamily: 'montserrat',
+                         fontWeight: FontWeight.bold))),
            ),
-           child: ElevatedButton(
-               onPressed: () {
-                 Navigator.pushNamed(context, '/display', arguments: map);
-                 /*showDialog(
-                   context: context,
-                   builder: (BuildContext context) {
-                     return  MyDialog( map: map);
-                   },
-                 );*/
-               },
-               child: const Text("Décomposition",
-                   style: TextStyle(
-                       fontFamily: 'montserrat',
-                       fontWeight: FontWeight.bold))),
-         ),
-          ],
+            ],
+          ),
         ),
 
       ),
